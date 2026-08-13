2c0393791bb23a85733a580d92d1b66c10a65fb8
Joanna <sjy9280@163.com>
2026-08-13 14:34:58 +0800

    feat:first init

---
+++ server.mjs
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+app.use((req, res) => {
+  res.send('Hello World');
+})
+
+app.listen(3001, () => {
+  console.log('Server is running on port 3001');
+});
+
+console.log('ESM');
